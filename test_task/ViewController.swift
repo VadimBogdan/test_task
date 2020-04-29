98e02fa976fa1096df788573cfca1a181e9bb3a8
Vadym Bogdan <vadymbog@gmail.com>
2020-04-29 10:14:42 +0300

    Initial Commit

---
+++ test_task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test_task
+//
+//  Created by Вадим on 29.04.2020.
+//  Copyright © 2020 Vadym Bogdan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
